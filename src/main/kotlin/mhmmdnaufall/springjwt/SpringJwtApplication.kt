a3615a5fd34530f4f209fca4eddb3de08630dac2
Muhammad Naufal <muhammadnaufaall@gmail.com>
2023-09-17 23:45:54 +0700

    project init

---
+++ src/main/kotlin/mhmmdnaufall/springjwt/SpringJwtApplication.kt
@@ -0,0 +1,11 @@
+package mhmmdnaufall.springjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJwtApplication>(*args)
+}
